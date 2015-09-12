1eccca5eaf38118f0a200f4c5e4ea14076791c66
Mashiro <alexander49538@gmail.com>
2015-09-12 10:44:12 +0800

    Update  socks.py to Version 1.5.6

--- socks.py
+++ socks.py
@@ -1,6 +1,6 @@
 """
 SocksiPy - Python SOCKS module.
-Version 1.5.4
+Version 1.5.6
 
 Copyright 2006 Dan-Haim. All rights reserved.
 
@@ -52,7 +52,7 @@ Modifications made by Anorov (https://github.com/Anorov)
 -Various small bug fixes
 """
 
-__version__ = "1.5.4"
+__version__ = "1.5.6"
 
 import socket
 import struct
@@ -149,7 +149,8 @@ wrapmodule = wrap_module
 def create_connection(dest_pair, proxy_type=None, proxy_addr=None,
                       proxy_port=None, proxy_rdns=True,
                       proxy_username=None, proxy_password=None,
-                      timeout=None, source_address=None):
+                      timeout=None, source_address=None,
+                      socket_options=None):
     """create_connection(dest_pair, *[, timeout], **proxy_args) -> socket object
 
     Like socket.create_connection(), but connects to proxy
@@ -162,11 +163,17 @@ def create_connection(dest_pair, proxy_type=None, proxy_addr=None,
     address before connecting (only for compatibility)
     """
     sock = socksocket()
+    if socket_options is not None:
+        for opt in socket_options:
+            sock.setsockopt(*opt)
     if isinstance(timeout, (int, float)):
         sock.settimeout(timeout)
     if proxy_type is not None:
         sock.set_proxy(proxy_type, proxy_addr, proxy_port, proxy_rdns,
                        proxy_username, proxy_password)
+    if source_address is not None:
+        sock.bind(source_address)
+
     sock.connect(dest_pair)
     return sock
 
@@ -625,6 +632,12 @@ class socksocket(_BaseSocket):
 
         dest_pair - 2-tuple of (IP/hostname, port).
         """
+        if len(dest_pair) != 2 or dest_pair[0].startswith("["):
+            # Probably IPv6, not supported -- raise an error, and hope
+            # Happy Eyeballs (RFC6555) makes sure at least the IPv4
+            # connection works...
+            raise socket.error("PySocks doesn't support IPv6")
+
         dest_addr, dest_port = dest_pair
 
         if self.type == socket.SOCK_DGRAM:
